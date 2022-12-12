3cbe901e5893b13d7e0da7b75da407c308ac9373
cccjjjjjj <113432966+cccjjjjjj@users.noreply.github.com>
2022-12-12 09:41:56 +0800

    Delete sql directory

--- sql/activity.sql
+++
@@ -1,8 +0,0 @@
-create table activity
-(
-    id   int auto_increment
-        primary key,
-    name varchar(250) not null
-);
-
-INSERT INTO new_quiz.activity (id, name) VALUES (1, 'Software Engineering');
